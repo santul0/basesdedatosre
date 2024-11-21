2931135217173966b056978f324cbaa0794740fc
Santulo <111528542+santul0@users.noreply.github.com>
2024-11-21 00:49:04 -0300

    universe.sql

--- Relational Database Projects freecodecamp/Celestial Bodies Database/universe.sql
+++ Relational Database Projects freecodecamp/Celestial Bodies Database/universe.sql
@@ -18,7 +18,7 @@ SET row_security = off;
 
 DROP DATABASE universe;
 --
--- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
+-- Nme: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
 --
 
 CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
